a94fb70eb90fc43a65467f327bd17cdcd608c5a2
HP <HP@DESKTOP-19J60DN>
2025-09-26 11:41:57 +0530

    First Commit Version1

---
+++ EmployeeManagementSystem/src/main/java/com/dao/ConnectionManager.java
@@ -0,0 +1,21 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/ems";
+		String username = "root";
+		String password = "root";
+		
+		Class.forName(driver);
+		Connection connection = DriverManager.getConnection(url, username, password);
+		connection.setAutoCommit(false);
+		return connection;
+	}
+
+}
